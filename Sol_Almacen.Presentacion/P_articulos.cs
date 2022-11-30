87a044a678337036a4895e6e2b0e2bb7830af3af
MAllena <MAllena@rosariobus.com.ar>
2022-11-30 11:12:59 -0300

    propiedades_articulos

---
+++ Sol_Almacen.Presentacion/P_articulos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_Almacen.Presentacion
+{
+    public class P_articulos
+    {
+        public int Codigo_ar { get; set; }
+        public string Descripcion_ar { get; set; }
+        public string Marca_ar { get; set; }
+        public int Codigo_um { get; set; }
+        public int Codigo_ca { get; set; }
+        public decimal stock_actual { get; set; }
+        public string FechaCrea_ar { get; set; }
+        public string FechaModifica_ar { get; set; }
+
+
+    }
+}
